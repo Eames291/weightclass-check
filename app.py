d3ea2a2b36e1440d98dbd4fdff63791bd006d174
Eames291 <141921665+Eames291@users.noreply.github.com>
2023-08-10 16:27:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+!pip install streamlit
+import streamlit as st
+import joblib
+joblib.dump(clf,'model_new')
+model = joblib.load('model_new')
+st.title("Non-Combative Weight Class Calculator")
+weight = st.slider('What is your weight?', 0, 100)
+height = st.slider('What is your height?', 0, 200)
+result = model.predict([[weight,height]])
+if st.button("Calculate"):
+  st.write(f"You are {result}")
+
